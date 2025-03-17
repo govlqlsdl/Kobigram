9279bd881cbad9fa83c97800efe091a8e5774216
LAPTOP-DHOJEMI6\석호빈 <govlqlsdl@naver.com>
2025-03-17 19:30:01 +0900

    [석호빈] - Kobigram 프로젝트 추가

---
+++ src/main/java/com/kobi/kobigram/KobigramApplication.java
@@ -0,0 +1,13 @@
+package com.kobi.kobigram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobigramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KobigramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kobi/kobigram/KobigramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobi.kobigram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KobigramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
